27d082bf3808fbb23c55e46caa5386a4cdefe2f9
Summersspark1750 <summersspark@outlook.com>
2026-02-16 05:17:47 +0800

    finsihed object handshake godot and unity

---
+++ Godot/Scripts/Receiver.gd
@@ -0,0 +1,10 @@
+extends Node
+class_name Receiver
+
+# Called when the node enters the scene tree for the first time.
+func OnCalled(): 
+	print ("Hiiiiiiiiiiiiiiiiiiiiiiii, hello recieved")
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
---
+++ Unity/Assign2Kenny/Assets/Scripts/Caller.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Caller : MonoBehaviour
+{
+    public Receiver receiver;   //
+
+    void Start()
+    {
+        Debug.Log("Calling...");
+        receiver.OnCalled();
+    }
+}
